60985785f2e3c1bb73c4cb0d317a809f1b56e7cd
Mikhail Mokhov <mikhail.mokhov@caseware.com>
2023-04-18 07:33:36 -0700

    work in progress

--- actions/properties/index.js
+++ actions/properties/index.js
@@ -1,15 +1,9 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
 
-
-const main = async () => {
+try {
   const tags = ["scope:server"]
-
   core.info(tags.toString());
-
   core.setOutput('tags', tags);
-};
-
-main().catch((e) => {
-  core.setFailed(e);
-});
+} catch (error) {
+  core.setFailed(error.message);
+}
